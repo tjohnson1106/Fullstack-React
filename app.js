e3bba84a477d0d760862e1d549a9ecd19adb5a65
ThomasJohnson <tjohnson1106@gmail.com>
2016-06-18 10:53:07 -0400

    passed down handleUpVote function to Product component

--- app.js
+++ app.js
@@ -1,68 +1,67 @@
-//ProductList passes down props to Product
 const ProductList = React.createClass({
-  render: function () {
-    const product = Data[0];
-    return (
-      <div className='ui items'>
-        <Product
-          id={product.id}
-          title={product.title}
-          description={product.description}
-          url={product.url}
-          votes={product.votes}
-          submitter_avatar_url={product.submitter_avatar_url}
-          product_image_url={product.product_image_url}
-        />
-      </div>
-    );
-  },
+  handleProductUpVote: function (productId) {
+console.log(productId + " was upvoted.");
+},
+render: function () {
+const products = Data.map((product) => {
+return (
+<Product
+key={'product-' + product.id}
+id={product.id}
+title={product.title}
+description={product.description}
+url={product.url}
+votes={product.votes}
+submitter_avatar_url={product.submitter_avatar_url}
+product_image_url={product.product_image_url}
+onVote={this.handleProductUpVote}
+/>
+);
+});
+return (
+<div className='ui items'>
+{products}
+</div>
+);
+},
 });
 
 const Product = React.createClass({
-  render: function () {
-    return (
-      <div className='item'>
-        <div className='image'>
-          <img src=
-      {this.props.product_image_url} />
-          <img src='images/products/image-aqua.png' />
-        </div>
-        <div className='middle aligned content'>
-           <div className='header'>
-            <a>
-              <i className='large caret up icon'></i>
-            </a>
-            {this.props.votes}
-          </div>
-          <div className='description'>
-            <a href={this.props.url}>
-              {this.props.title}
-            </a>
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two weeks.</p>
-          </div>
-          <div className='extra'>
-            <span>Submitted by:</span>
-            <img
-              className='ui avatar image'
-              src=
-      {this.props.submitter_avatar_url}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  },
+  handleUpVote: function () {
+this.props.onVote(this.props.id);
+},
+render: function () {
+return (
+<div className='item'>
+<div className='image'>
+<img src={this.props.product_image_url} />
+</div>
+<div className='middle aligned content'>
+<div className='header'>
+<a onClick={this.handleUpVote}>
+<i className='large caret up icon'></i>
+</a>
+{this.props.votes}
+</div>
+<div className='description'>
+<a href={this.props.url}>
+{this.props.title}
+</a>
+</div>
+<div className='extra'>
+<span>Submitted by:</span>
+<img
+className='ui avatar image'
+src={this.props.submitter_avatar_url}
+/>
+</div>
+</div>
+</div>
+);
+},
 });
-
 //native HTML elements always start with a lowercase letter whereas React component names always start with an uppercase letter
 
-ReactDOM.render(
-  <ProductList />,
+ReactDOM.render( < ProductList / > ,
   document.getElementById('content')
 );
-
-
-
-
-
